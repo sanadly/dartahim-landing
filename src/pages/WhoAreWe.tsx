1f0aeabb1e58e3643d7b9400789a7093cdec449f
Sanad Ghamu <76404382+sanadly@users.noreply.github.com>
2025-03-10 03:22:31 +0100

    deleted the header in whoarewe page

--- src/pages/WhoAreWe.tsx
+++ src/pages/WhoAreWe.tsx
@@ -106,7 +106,8 @@ const WhoAreWe = () => {
           </div>
           {/* Milestones */}
           <div className="mb-20">
-            /*<h2 className="text-3xl font-bold mb-8 text-center">مراحل تطورنا</h2>*/
+          {/* Header here */}
+
             <div className="relative">
               {/* Timeline Line */}
               <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-1 bg-primary/20 hidden md:block"></div>
